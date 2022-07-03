4886a33cb311033de5d7fdf7249962e2bd2aab0c
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-07-03 14:59:51 -0400

    Create computerPlay function and computerPlay var

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
 let playerSelection=prompt("Rock, paper, or scissors?");
 playerSelection=playerSelection.toLowerCase()
-console.log(playerSelection);
+console.log("Player selection:",playerSelection);
+
+function computerPlay(){
+    let choice=Math.floor(Math.random()*3)+1;
+    console.log("Random number:",choice);
+    if (choice===1){
+        return "rock"
+    } else if (choice===2){
+        return "paper"
+    } else if (choice===3) {
+        return "scissors"
+    }
+}
+let computerSelection=computerPlay();
+console.log("Computer selection:",computerSelection);
