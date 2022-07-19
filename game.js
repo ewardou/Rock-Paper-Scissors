66e1c6750fafaa17166d530b536ee9352c1ffbf3
Eduardo Sánchez <eduardosr1599@gmail.com>
2022-07-19 16:20:28 -0400

    Add query selectors for score and message boxes

--- game.js
+++ game.js
@@ -1,7 +1,28 @@
 
-let userScore;
-let computerScore;
-let playerSelection
+let userScore=0;
+let computerScore=0;
+let playerSelection;
+//let computerSelection;
+
+
+const btn=document.querySelectorAll(".container button")
+btn.forEach((button) =>{
+    button.addEventListener("click",(event)=>{
+        if(event.target.className==="rock"){
+            playerSelection="rock";
+        } else if (event.target.className==="paper"){
+            playerSelection="paper";
+        } else if (event.target.className==="scissors"){
+            playerSelection="scissors"
+        };
+       playRound(playerSelection,computerPlay());
+    });
+});
+
+const msg=document.querySelector(".message");
+const userScoreDom=document.querySelector(".user-score");
+const computerScoreDom=document.querySelector(".cpu-score");
+
 //Function to randomly select computer's choice
 function computerPlay(){
     let choice=Math.floor(Math.random()*3)+1;
@@ -19,61 +40,54 @@ function playRound(playerSelection, computerSelection){
     //When player wins
     if (playerSelection==="rock" && computerSelection==="scissors"){
         userScore++;
-        return "You win! Rock beats scissors";
+        userScoreDom.textContent=userScore;
+        return msg.textContent="You win! Rock beats scissors";
     }else if (playerSelection==="scissors" && computerSelection==="paper"){
         userScore++;
-        return "You win! Scissors beats paper";
+        userScoreDom.textContent=userScore;
+        return msg.textContent="You win! Scissors beats paper";
     }else if (playerSelection==="paper" && computerSelection==="rock"){
         userScore++;
-        return "You win! Paper beats rock";
+        userScoreDom.textContent=userScore;
+        return msg.textContent="You win! Paper beats rock";
 
     //When computer wins    
     }else if (computerSelection==="rock" && playerSelection==="scissors"){
         computerScore++;
-        return "You lose! Rock beats scissors";
+        computerScoreDom.textContent=computerScore;
+        return msg.textContent="You lose! Rock beats scissors";
     }else if (computerSelection==="scissors" && playerSelection==="paper"){
         computerScore++;
-        return "You lose! Scissors beats paper";
+        computerScoreDom.textContent=computerScore;
+        return msg.textContent="You lose! Scissors beats paper";
     }else if (computerSelection==="paper" && playerSelection==="rock"){
         computerScore++;
-        return "You lose! Paper beats rock";
+        computerScoreDom.textContent=computerScore;
+        return msg.textContent="You lose! Paper beats rock";
     }else {
-        return "It's a tie"
+        return msg.textContent="It's a tie"
     }
 }
 // Game function to start a game of rock, paper, scissors
-function game(){
+/*function game(){
     userScore=0;
     computerScore=0;
 
-//    for (let i=1; (userScore<5) && (computerScore<5) ;i++){
-//        let playerSelection=prompt("Rock, paper, or scissors?");
-//        playerSelection=playerSelection.toLowerCase();
+    for (let i=1; (userScore<5) && (computerScore<5) ;i++){
+        let playerSelection=prompt("Rock, paper, or scissors?");
+        playerSelection=playerSelection.toLowerCase();
         let computerSelection=computerPlay();
         console.log(playRound(playerSelection,computerSelection));
         console.log("User score:",userScore);
         console.log("Computer score:",computerScore)
-//        }
+        }
 
     if (userScore>computerScore){
-        console.log("You have won!");
+        return msg.textContent="You have won!";
     } else if(computerScore>userScore){
-        console.log("You have lost") ;
+        return msg.textContent="You have lost" ;
     } else{
-        console.log("You have drawn");
+        return msg.textContent="You have drawn";
     }
-}
+} */
 
-
-const btn=document.querySelectorAll(".container button")
-btn.forEach((button) =>{
-    button.addEventListener("click",(event)=>{
-        if(event.target.className==="rock"){
-            playerSelection="rock";
-        } else if (event.target.className==="paper"){
-            playerSelection="paper";
-        } else if (event.target.className==="scissors"){
-            playerSelection="scissors"
-        }
-    });
-});
